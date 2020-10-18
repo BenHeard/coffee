7e55033955f23bd588091e328e875603dc659aaf
Raticos <51394970+Raticos@users.noreply.github.com>
2020-10-18 17:29:20 +1030

    r

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+
+        <form action="includes/signup.inc.php" method="POST">
+            <input type="text" name="first" placeholder="Firstname">
+            <br>
+            <input type="text" name="last" placeholder="Lastname">
+            <br>
+            <input type="text" name="email" placeholder="E-mail">
+            <br>
+            <input type="text" name="uid" placeholder="Username">
+            <br>
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <button type="submit" name="submit">Sign up</button>
+        
+
+    </body>   
+</html>
